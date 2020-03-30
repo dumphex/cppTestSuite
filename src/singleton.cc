3c4346bcac33a7d2f723b73062b15186ca3ad603
dumphex <dumphex@163.com>
2020-03-31 00:51:14 +0800

    Add atomic for DCLP at singleton.cc

--- src/singleton.cc
+++ src/singleton.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mutex>
+#include <atomic>
 
 // SingletonEager
 class SingletonEager {
@@ -31,9 +32,11 @@ class SingletonLazy {
     std::cout << __FUNCTION__ << " is caled. " << std::endl;
   }
 
-#if 1
-  static SingletonLazy& getInstance() {
-    std::lock_guard<std::mutex> lock(m_mtx);
+#define ATOMIC
+
+#ifndef ATOMIC
+  static SingletonLazy & getInstance() {
+    std::lock_guard<std::mutex> lck(m_mtx);
     if(m_instance == nullptr) {
       m_instance = new SingletonLazy();
     }
@@ -41,9 +44,10 @@ class SingletonLazy {
     return *m_instance;
   }
 #else
+#if 0
   static SingletonLazy& getInstance() {
     if(m_instance == nullptr) {
-      std::lock_guard<std::mutex> lock(m_mtx);
+      std::lock_guard<std::mutex> lck(m_mtx);
       if(m_instance == nullptr) {
         m_instance = new SingletonLazy();
       }
@@ -51,11 +55,32 @@ class SingletonLazy {
 
     return *m_instance;
   }
+#else
+  static SingletonLazy & getInstance() {
+    SingletonLazy *p = m_instance.load(std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_acquire);
+    if (p == nullptr) {
+      std::lock_guard<std::mutex> lck(m_mtx);
+      p = m_instance.load(std::memory_order_relaxed);
+      if (p == nullptr) {
+        p = new SingletonLazy();
+        std::atomic_thread_fence(std::memory_order_release);
+        m_instance.store(p, std::memory_order_relaxed);
+      }
+    }
+
+    return *p;
+  }
+#endif
 #endif
 
  private:
   static std::mutex m_mtx;
-  static SingletonLazy* m_instance;
+#ifndef ATOMIC
+  static SingletonLazy *m_instance;
+#else
+  static std::atomic<SingletonLazy *> m_instance;
+#endif
 
   SingletonLazy() {
     std::cout << __FUNCTION__ << " is caled. " << std::endl;
@@ -66,7 +91,11 @@ class SingletonLazy {
 };
 
 std::mutex SingletonLazy::m_mtx;
-SingletonLazy* SingletonLazy::m_instance = nullptr;
+#ifndef ATOMIC
+SingletonLazy *SingletonLazy::m_instance = nullptr;
+#else
+std::atomic<SingletonLazy *> SingletonLazy::m_instance(nullptr);
+#endif
 
 // local static
 class SingletonLocalStatic {
